a2847ade9072a905f556751139d0c2334535c02f
boullle <bill.bouilly1@gmail.com>
2025-11-04 17:13:06 +0100

    exercice 2

--- Country.php
+++ Country.php
@@ -14,8 +14,32 @@ Class Country {
         $this->population = $population;
         $this->continent = $continent;
     }
+    /*getters*/
+    public function getName(){
+        return $this->name;
+    }
+    public function getCapital(){
+        return $this->capital;
+    }
+    public function getPopulation(){
+        return $this->population;
+    }
+    public function getContinent(){
+        return $this->continent;
+    }
+    /*setters*/
+    public function setCapital($capital){
+        $this->capital = $capital;
+    }
+    public function setPopulation($population){
+        $this->population = $population;
+    }
+    public function setContinent($continent){
+        $this->continent = $continent;
+    }
+
     public function getInfo()
     {
-            return "<br>le pays".$this->name." a pour capitale ".$this->capital." et une population de ".$this->population." et est sur le continent de ".$this->continent."<br>";
+            return "<br>le pays ".$this->name." a pour capitale ".$this->capital." et une population de ".$this->population." et est sur le continent de ".$this->continent."<br>";
     }
 }
--- test_country.php
+++ test_country.php
@@ -7,3 +7,15 @@ echo $France->getInfo();
 echo $Algerie->getInfo();
 echo $Belgique->getInfo();
 
+echo "<br>".$France->getCapital();
+echo "<br>".$Algerie->getContinent();
+echo "<br>".$Belgique->getContinent();
+
+$France->setCapital("Toulouse");
+echo $France->getInfo();
+
+echo "mon tableau";
+$countries = [$France,$Algerie,$Belgique];
+foreach ($countries as $country) {
+    echo $country->getInfo();
+}
